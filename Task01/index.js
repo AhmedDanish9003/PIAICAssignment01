ad26e98593a31f32d4807dcd8ba8eb023ed537a2
AhmedDanish9003 <ahmed.danish9003@gmail.com>
2023-07-23 14:27:50 +0500

    Initial Commit

---
+++ Task01/index.js
@@ -0,0 +1,3 @@
+var nameTask01 = "Ahmed Danish";
+console.log('My Name is ', nameTask01);
+console.log("\n \tHello ".concat(nameTask01, ", would you like to learn some Python today"));
---
+++ Task01/index.ts
@@ -0,0 +1,6 @@
+const nameTask01:string="Ahmed Danish";
+
+let num: number= 1;
+
+if( num<=1)
+console.loh()
